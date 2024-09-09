ecf0a8984b21e79a8c0848cc5228f0285a18f881
Kamyar Bashar <kamyar.bashar@gmail.com>
2024-09-09 11:28:44 +0330

    "Updated Tailwind config to include new HTML templates, added new CSS classes to tailwindstyles.css, and added CSS blocks to HTML templates"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   content: [
+    //todolist 
+
+    './todolist/templates/todolist/layout.html',
     './todolist/templates/todolist/today.html',
     './todolist/templates/todolist/starred.html',
     './todolist/templates/todolist/schedul.html',
     './todolist/templates/todolist/completed.html',
     './todolist/templates/todolist/all.html',
+    './todolist/templates/todolist/nav.html'
+
+    
   ],
   theme: {
     extend: {},
