bbc540172e59e3990fcdade57e82f03b76f71435
Kamyar Bashar <kamyar.bashar@gmail.com>
2024-09-12 13:40:22 +0330

    "Refactored templates and CSS files in clock, shared, thoughts, and todolist apps; updated URLs and views in thoughts app."

--- thoughts/urls.py
+++ thoughts/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'thoughts'
 
 urlpatterns = [
-    path('', views.Thoughts.as_view(), name="thoughts")
+    path('', views.Thoughts.as_view(), name="thoughts"),
+    path('write', views.Write.as_view(), name="write"),
+    path('library', views.Library.as_view(), name="library"),
+    path('categories', views.Categories.as_view(), name="categories"),
+    path('archived', views.Archived.as_view(), name="archived")
 ]
--- thoughts/views.py
+++ thoughts/views.py
@@ -5,3 +5,19 @@ from django.views import View
 class Thoughts(View):
     def get(self, request):
         return render(request, 'thoughts/thoughts.html')
+    
+class Write(View):
+    def get(self, request):
+        return render(request, 'thoughts/write.html')
+    
+class Library(View):
+    def get(self, request):
+        return render(request, 'thoughts/library.html')
+
+class Categories(View): 
+    def get(self, request):
+        return render(request, 'thoughts/categories.html')
+
+class Archived(View):
+    def get(self, request):
+        return render(request, 'thoughts/archived.html')
